25a7f78a094200be0ae49f92daf685ae5de0bc8e
Noah Salaway <nms348@nyu.edu>
2017-03-08 18:09:02 -0500

    added scorekeeping

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -14,17 +14,18 @@ public class ButtonScript : MonoBehaviour {
 	[SerializeField]
 	Transform spawnpoint;
 
+    GameObject scoreboard;
 
+    bool firstBall = true;
 
 	// Use this for initialization
 	void Start () {
-		
+        scoreboard = GameObject.Find("Scoreboard");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (ball != null)
-		Debug.Log (ball.GetComponent<SphereCollider> ().material.staticFriction);
+
 	}
 
 	void HandHoverUpdate(Hand hand){
@@ -32,9 +33,10 @@ public class ButtonScript : MonoBehaviour {
 			if (ball != null) {
 				Destroy (ball.gameObject);
 			}
-			ball = Instantiate (ballprefab, spawnpoint.transform.position, Quaternion.identity);
-
-
+            ball = Instantiate(ballprefab, spawnpoint.transform.position, Quaternion.identity);
+            if (!firstBall)
+                scoreboard.GetComponent<Scorekeeper>().NextFrame();
+            firstBall = false;
 		}
 	}
 
