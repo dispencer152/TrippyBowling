2b932bef497e63a4e54d4a3ba7edfe02653afa60
Madeline Falacci <mfalacci@gmail.com>
2017-05-03 17:41:23 -0400

    I made an interaction for the big pins that needs testing (lots of change to prefabs and scripts)

--- Assets/Scripts/treeGenerator.cs
+++ Assets/Scripts/treeGenerator.cs
@@ -4,23 +4,26 @@ using System.Collections;
 
 public class treeGenerator : MonoBehaviour {
 
-	public GameObject pinPrefab;
+	public GameObject bigpin1;
+	public GameObject bigpin2;
+	public GameObject bigpin3; 
 
 	//public GameObject treePrefabColor;
 
-	List<GameObject> listofPins = new List<GameObject>();
-
+	List<GameObject> listofPins1 = new List<GameObject>();
+	List<GameObject> listofPins2 = new List<GameObject>();
+	List<GameObject> listofPins3 = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		while (listofPins.Count < 200) {
-			GameObject newPin = (GameObject)Instantiate(pinPrefab, new Vector3 (Random.Range (-360f,90f), Random.Range (-5f,6f), Random.Range (-400f,100f)), Quaternion.Euler (0f,(Random.Range(0,360)),0f));
+		while (listofPins1.Count < 60) {
+			GameObject newPin = (GameObject)Instantiate(bigpin1, new Vector3 (Random.Range (-360f,90f), Random.Range (-5f,6f), Random.Range (-400f,100f)), Quaternion.Euler (0f,(Random.Range(0,360)),0f));
 			newPin.GetComponent<MeshRenderer>().material.color = new Color (Random.value, Random.value, Random.value, 1f);
 			newPin.GetComponent<MeshRenderer> ().material.EnableKeyword ("_EMISSION");
 			newPin.GetComponent<MeshRenderer> ().material.SetColor ("_Emission", Color.blue);
 			newPin.transform.localScale *= Random.Range (5, 50);
-			pinPrefab.GetComponent<Renderer>().sharedMaterial.color = new Color (Random.value, Random.value, Random.value, 1f);
-			listofPins.Add (newPin);
+			bigpin1.GetComponent<Renderer>().sharedMaterial.color = new Color (Random.value, Random.value, Random.value, 1f);
+			listofPins1.Add (newPin);
 			if ((newPin.transform.position.x > -16f) && (newPin.transform.position.x < 9f)){
 				Destroy (newPin);
 			}
@@ -29,6 +32,36 @@ public class treeGenerator : MonoBehaviour {
 			}
 		}
 
+		while (listofPins2.Count < 60) {
+			GameObject newPin = (GameObject)Instantiate(bigpin2, new Vector3 (Random.Range (-360f,90f), Random.Range (-5f,6f), Random.Range (-400f,100f)), Quaternion.Euler (0f,(Random.Range(0,360)),0f));
+			newPin.GetComponent<MeshRenderer>().material.color = new Color (Random.value, Random.value, Random.value, 1f);
+			newPin.GetComponent<MeshRenderer> ().material.EnableKeyword ("_EMISSION");
+			newPin.GetComponent<MeshRenderer> ().material.SetColor ("_Emission", Color.blue);
+			newPin.transform.localScale *= Random.Range (5, 50);
+			bigpin2.GetComponent<Renderer>().sharedMaterial.color = new Color (Random.value, Random.value, Random.value, 1f);
+			listofPins2.Add (newPin);
+			if ((newPin.transform.position.x > -16f) && (newPin.transform.position.x < 9f)){
+				Destroy (newPin);
+			}
+			if ((newPin.transform.position.z >-28f ) && (newPin.transform.position.z < 12f)){
+				Destroy (newPin);
+			}
+		}
+		while (listofPins3.Count < 60) {
+			GameObject newPin = (GameObject)Instantiate(bigpin3, new Vector3 (Random.Range (-360f,90f), Random.Range (-5f,6f), Random.Range (-400f,100f)), Quaternion.Euler (0f,(Random.Range(0,360)),0f));
+			newPin.GetComponent<MeshRenderer>().material.color = new Color (Random.value, Random.value, Random.value, 1f);
+			newPin.GetComponent<MeshRenderer> ().material.EnableKeyword ("_EMISSION");
+			newPin.GetComponent<MeshRenderer> ().material.SetColor ("_Emission", Color.blue);
+			newPin.transform.localScale *= Random.Range (5, 50);
+			bigpin1.GetComponent<Renderer>().sharedMaterial.color = new Color (Random.value, Random.value, Random.value, 1f);
+			listofPins3.Add (newPin);
+			if ((newPin.transform.position.x > -16f) && (newPin.transform.position.x < 9f)){
+				Destroy (newPin);
+			}
+			if ((newPin.transform.position.z >-28f ) && (newPin.transform.position.z < 12f)){
+				Destroy (newPin);
+			}
+		}
 	}
 
 
